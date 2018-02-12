85053d666f0d8d351da564e4c6e17a65ed882b86
Unknown <ramuzat.noelie@gmail.com>
2018-02-12 17:34:39 +0100

    Mission Interpreter with good parsing JSON

---
+++ src/mission_interpreter.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+from BathyBoatNav.srv import *
+from collections import defaultdict
+import rospy
+import json
+
+global missions, nbrMissions, longitudes, latitudes, nbrMissionsSent
+latitudes = defaultdict(list)
+longitudes = defaultdict(list)
+missions = defaultdict(list)
+nbrMissions = 0
+nbrMissionsSent = 0
+
+def readFile():
+	jsonFile = open("/home/noelie/Documents/ProjetGuerledan/Noelie/mission1.json", "r")
+	jsonDict = json.loads(jsonFile.read())
+	global missions, nbrMissions, longitudes, latitudes
+	for i in jsonDict["missions"] :	
+		if i['type'] == "WayPoint" :
+			missions[nbrMissions] = False;
+			for j in i['wayPoints'] :
+				latitudes[nbrMissions].append(j['latitude'])
+				longitudes[nbrMissions].append(j['longitude'])		
+		else :
+			missions[nbrMissions] = True;
+		nbrMissions += 1
+	print longitudes
+	print latitudes
+	
+def sendPointService(req):
+	global missions, nbrMissions, longitudes, latitudes, nbrMissionsSent
+	res = {}
+	res['longitude'] = []
+	res['latitude'] = []
+	if nbrMissionsSent < nbrMissions and longitudes[nbrMissionsSent]:
+		res['isRadiale'] = missions[nbrMissionsSent]
+		if not missions[nbrMissionsSent]:
+			res['longitude'].append(longitudes[nbrMissionsSent].pop(0))
+			res['latitude'].append(latitudes[nbrMissionsSent].pop(0))
+			print longitudes
+			print latitudes
+			print res['longitude']
+			print res['latitude']
+			
+			if not longitudes[nbrMissionsSent]:
+				nbrMissions = nbrMissions - nbrMissionsSent
+				nbrMissionsSent += 1
+	
+			res['nbrMissionNext'] = nbrMissions - 1
+		else: 
+			pass
+	print res
+	return res
+		
+
+if __name__ == "__main__":
+	readFile()
+	rospy.init_node('mission_interpreter')
+	s = rospy.Service('/next_goal', next_goal, sendPointService)
+	rospy.spin()
+		
+		
+
