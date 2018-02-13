6fee3aac531a40d6703e70e8901405c5d41439d0
lallemfa <fabrice.lallement@ensta-bretagne.org>
2018-02-13 23:03:08 +0100

    Teleop launch file

--- src/pololu.cpp
+++ src/pololu.cpp
@@ -12,12 +12,24 @@
 
 using namespace std;
 
-double u_yaw;
+double u_throttle, u_yaw;
 double left_mot, right_mot;
+
+string path;
+string channel;
+int gap;
 
 void chatCallback(const geometry_msgs::Twist::ConstPtr& msg)
 {
-    u_yaw 	= msg->angular.z;
+    u_throttle 	= msg->linear.x;
+    u_yaw 		= msg->angular.z;
+}
+
+void init_servo(int fd)
+{
+	maestroSetTarget(fd, 0, 6000);
+	maestroSetTarget(fd, 1, 6000);
+	sleep(2);
 }
 
 
@@ -26,41 +38,32 @@ int main(int argc, char *argv [])
 	ros::init(argc, argv, "pololu");
     ros::NodeHandle n;
     
-        // Parametres initiaux
-    //n.param<double>("Pos_x", x[0], 0);
-
     ros::Rate loop_rate(10);
-
+    
+        // Parametres initiaux
+    
+    n.param<string>("Path", path, "/dev/pololu");
+    n.param<string>("Msg_name", channel, "/key_vel");
+    n.param<int>("Turn_gap", gap, 100);
 
     	// Connection to Maestro
-	int fd = maestroConnect("/dev/ttyACM0");
+
+   	//int fd = maestroConnect(path.c_str());
 	printf("Pololu connected\n");
+	//init_servo(fd);
 
-	maestroSetTarget(fd, 0, 8000);
-	maestroSetTarget(fd, 1, 8000);
-
-	sleep(2);
-
-	maestroSetTarget(fd, 0, 4000);
-	maestroSetTarget(fd, 1, 4000);
-
-	sleep(2);
-
-	maestroSetTarget(fd, 0, 6000);
-	maestroSetTarget(fd, 1, 6000);
-
-		// Subscribe msgs
-    ros::Subscriber cons_sub = n.subscribe("cons_boat", 1000, chatCallback);
+			// Subscribe msgs
+    ros::Subscriber cons_sub = n.subscribe(channel, 1000, chatCallback);
 
 	while(ros::ok())
 	{
-		left_mot 	= 6000 + u_yaw*2000;
-		right_mot 	= 6000 - u_yaw*2000;
+		left_mot 	= 6000 + u_throttle*(2000 - gap) + u_yaw*gap;
+		right_mot 	= 6000 + u_throttle*(2000 - gap) - u_yaw*gap;
 
-		ROS_INFO("Info -> %lf | (%lf, %lf)\n", u_yaw, left_mot, right_mot);
+		ROS_INFO("\nu_throttle -> %lf\n u_yaw -> %lf\n Cons_pololu = (%lf, %lf)\n", u_throttle, u_yaw, left_mot, right_mot);
 
-		maestroSetTarget(fd, 0, left_mot);
-		maestroSetTarget(fd, 1, right_mot);
+		//maestroSetTarget(fd, 0, left_mot);
+		//maestroSetTarget(fd, 1, right_mot);
 
 		ros::spinOnce();
         loop_rate.sleep();
